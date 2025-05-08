fdff645891b3610d0682513b2cb6c7f8204f7014
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2025-05-08 16:50:22 +0700

    delete incorrect ans

--- Running Sum of 1d Array.c
+++
@@ -1,9 +0,0 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* runningSum(int* nums, int numsSize, int* returnSize) {
-    int i, j, sum;
-    for (i = 0; i < numSize; i++) {
-        
-    }
-}
