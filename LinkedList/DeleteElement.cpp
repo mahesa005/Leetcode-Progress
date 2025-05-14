348d72920431f3e35f5204d9427918bf92dc3525
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2025-05-14 16:26:57 +0700

    14/05/2025

---
+++ LinkedList/DeleteElement.cpp
@@ -0,0 +1,17 @@
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (node->next->next == NULL) {
+            node->val = node->next->val;
+            node->next = NULL;
+            return;
+        }
+        node->val = node->next->val;
+        deleteNode(node->next);
+    }
+};
