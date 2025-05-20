fdba26cbb6c66bf884769bbe44dcbb64babb1913
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2025-05-20 18:15:34 +0700

    20/05/2025 (2)

---
+++ DFS/max-depth-of-binary-tree.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int max = 0;
+        if (!root) return NULL;
+        else {
+            max += 1 + std::max(maxDepth(root->left), maxDepth(root->right));
+        }
+        return max;
+    }
+};
