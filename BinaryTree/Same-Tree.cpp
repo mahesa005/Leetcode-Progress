75892d0f2459a8a3a752bfeb6aa03a957b053be0
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2025-05-25 21:32:24 +0700

    25/05/2025

--- BinaryTree/Same-Tree.cpp
+++ BinaryTree/Same-Tree.cpp
@@ -12,21 +12,17 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL && q == NULL) {
+        if (!p && !q) {
             return true;
         }
-        else if (p == NULL) {
+        else if (!p && q || p && !q) {
             return false;
         }
-        else if (q == NULL) {
+        else if (p->val != q->val) {
             return false;
         }
         else {
-            if (p->val == q-> val) {
-                isSameTree(p->left, q->left);
-                return isSameTree(p->right, q->right);
-            }
+            return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
         }
-        return false;
     }
 };
